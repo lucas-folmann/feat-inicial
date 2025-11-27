89cc09fdce765e6b64930fba242a06011c476da6
lucas-folmann <lucas.folmann@grupoboticario.com.br>
2025-11-27 15:34:20 -0300

    feat: Add ESLint and Jest configuration files for improved code quality and testing

---
+++ eslint.config copy.mjs
@@ -0,0 +1,29 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {files: ['**/*.{js,ts}']},
+  {languageOptions: {globals: globals.node}},
+  {
+    rules: {
+      semi: ['error', 'always'],
+      '@typescript-eslint/no-explicit-any': ['warn', {ignoreRestArgs: true}],
+      'comma-dangle': ['error', 'never']
+    }
+  },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      'coverage',
+      '**/*.config.{js,ts}',
+      'tsconfig.json'
+      // @scf-region eslint-ignore
+      // @end-scf-region
+    ]
+  }
+];
